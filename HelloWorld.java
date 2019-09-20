ef546bfb71145cc59c44fa52c7e6479e74ef3b81
Jonathan Reymond <jonrey@romandie.com>
2019-09-20 12:25:16 +0200

    salut

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
+  public static int computeUltimateAnswer() {
+    return 6 * 7;
+  }
   public static void main(String[] args) {
-    public static int computeUltimateAnswer() {
-	return 6 * 7;
-}
+   
     System.out.println("Hello world");
     System.out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
   }
