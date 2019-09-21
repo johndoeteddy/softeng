046b83f2a632381cfdc5b04d59123215c0096735
Jonathan Reymond <jonrey@romandie.com>
2019-09-21 15:45:28 +0200

    avec gradle

---
+++ src/main/java/swenggy/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+  public static int computeUltimateAnswer() {
+
+    System.out.println("still searching...");
+
+    return 7 * 6;
+  }
+  public static void main(String[] args) {
+   
+    System.out.println("Hello sweng");
+    System.out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
+  }
+}
